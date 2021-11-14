a34ce7ac06e3affa60afb6d5052460d628f82be1
Jelle van der Waa <jelle@vdwaa.nl>
2021-11-14 16:21:27 +0100

    Show the leftover sets

--- rowing-counter.ino
+++ rowing-counter.ino
@@ -35,28 +35,31 @@ void setup() {
     FastLED.showColor(CRGB::Black);
 }
 
+void shownumber(unsigned int number, CRGB color) {
+	int first  = number / 10;
+	int second = number % 10;
+
+	for (int i = 0; i < numleds; i++) {
+		leds[i] = CRGB::Black;
+	}
+
+	for (int y = 0; y < 5; y++) {
+		if (first) {
+			leds[y*5] = color;  // digit 1 or nothing
+		}
+
+		for (int x = 0; x < 3; x++) {
+			if (digits[y*30 + second*3 + x]) {
+				leds[y*5 + first + 1 + x] = color;
+			}
+		}
+	}
+	FastLED.show();
+}
+
 void countdown(unsigned int seconds, CRGB color) {
 	for (unsigned int n = seconds; n > 0; n--) {
-		int first  = n / 10;
-		int second = n % 10;
-
-		for (int i = 0; i < numleds; i++) {
-			leds[i] = CRGB::Black;
-		}
-
-		for (int y = 0; y < 5; y++) {
-			if (first) {
-				leds[y*5] = color;  // digit 1 or nothing
-			}
-
-			for (int x = 0; x < 3; x++) {
-				if (digits[y*30 + second*3 + x]) {
-					leds[y*5 + first + 1 + x] = color;
-				}
-			}
-		}
-		FastLED.show();
-
+		shownumber(n, color);
 		delay(1000);
 	}
 }
@@ -80,6 +83,9 @@ void loop() {
 		// Start
 		countdown(10, CRGB::Green);
 		for (unsigned int sets = 10; sets > 0; sets--) {
+			// Show current sets
+			shownumber(sets, CRGB::Cyan);	
+			delay(1000);
 			// Row
 			countdown(20, CRGB::Red);
 			// Cooldown
